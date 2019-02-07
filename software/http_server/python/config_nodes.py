8982669cb5c7550ba2bc35439263923c074e8c4a
Peter Maerki <45595002+petermaerki@users.noreply.github.com>
2019-02-07 18:16:57 +0100

    Update config_nodes.py

--- software/http_server/python/config_nodes.py
+++ software/http_server/python/config_nodes.py
@@ -76,11 +76,11 @@ listLabs.append({
   # ;config_app.setFixtemp(27.0)
   # ;config_app.iPollForWlanInterval_ms=60*60*1000
   # ;config_app.setOff()
-  GIT_TAGS: 'heads/master;5;config_app.iPollForWlanInterval_ms=10*60*1000',
+  GIT_TAGS: 'heads/master;5;config_app.iPollForWlanInterval_ms=2*60*1000',
   NODES: (
     ('20181217_19', ';config_app.setFixtemp(25.0)'),
     ('20181217_20', ';config_app.setFixtemp(25.0)'),
-    ('20181217_25', ';config_app.setFixtemp(23.0)'),
+    ('20181217_25', ';config_app.setFixtemp(24.0)'),
   )
 })
 
