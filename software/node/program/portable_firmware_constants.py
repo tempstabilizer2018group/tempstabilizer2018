0b384b263ccdbf0e7309b8f281985b79b5b81891
Hans Maerki <hans@maerki.com>
2019-02-09 22:00:14 +0100

    portable_firmware_constants.py

--- software/node/program/portable_firmware_constants.py
+++ software/node/program/portable_firmware_constants.py
@@ -27,6 +27,18 @@ strFILENAME_VERSION = 'VERSION.TXT'
 # See: https://github.com/tempstabilizer2018group/temp_stabilizer_2018/blob/master/software_rpi/rpi_root/etc/hostapd/hostapd.conf
 strWLAN_SSID = 'TempStabilizer2018'
 strWLAN_PW = 'wmm_enabled'
+iWLAN_ScanTime_ms = 1500
+iWLAN_Channel = 0 # All channels
+
+# Reboot: slow blink, sharp
+iLedReboot_pwm_hz = 2
+iLedReboot_duty_1023 = 50
+# Scan: blink, dark
+iLedWlanScan_pwm_hz = 10
+iLedWlanScan_duty_1023 = 100
+# Connected: blink, bright
+iLedWlanConnected_pwm_hz = 10
+iLedWlanConnected_duty_1023 = 512
 
 # See: https://github.com/tempstabilizer2018group/temp_stabilizer_2018/blob/master/software_rpi/rpi_root/etc/dhcpcd.conf
 strGATEWAY_PI = '192.168.4.1'
