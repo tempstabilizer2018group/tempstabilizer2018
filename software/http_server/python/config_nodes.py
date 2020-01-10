008f2a6ba147fdcead607d051948ec0f2021b248
Hans Märki <buhtig.hans.maerki@ergoinfo.ch>
2020-01-10 10:52:32 +0100

    Update config_nodes.py

--- software/http_server/python/config_nodes.py
+++ software/http_server/python/config_nodes.py
@@ -71,7 +71,7 @@ listLabs.append({
   LAB_NAME: 'Hoenggerberg',
   RESPONSIBLE: 'Alexander Eichler',
   GIT_REPO: 'tempstabilizer2018group/tempstabilizer2018',
-  GIT_TAGS: 'heads/master;4;config_app.iPollForWlanInterval_ms=20*60*1000;config_app.fPowerOffsetRangeOk_W=0.8',
+  GIT_TAGS: 'heads/master;5;config_app.iPollForWlanInterval_ms=20*60*1000;config_app.fPowerOffsetRangeOk_W=0.8',
   NODES: (
     ('20181217_17', ''),
     ('20181217_20', ''),
