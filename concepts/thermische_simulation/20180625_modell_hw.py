ed5c37cc6eefae96414192442866095cd58c8441
peter.maerki <git.peter@ergoinfo.ch>
2020-03-10 22:01:38 +0100

    .

--- concepts/thermische_simulation/20180625_modell_hw.py
+++ concepts/thermische_simulation/20180625_modell_hw.py
@@ -1,13 +1,13 @@
 # -*- coding: utf-8 -*-
 
-# Thermisches Modell der Hardware gemaess 20180625_modell_hw.png
+# Thermisches Modell der Hardware
 
-fTUmgebung_C = 20.0 # Umgebungstemperatur in Celsius
-fCH = 0.01 # Ws/K Waermekapazität beim FET, Energie pro Kelvin
-fRH = 25.0 # K/W Wärmewiderstand beim FET zur Umgebung.
-fRHO = 238.0 # K/W Waermewiderstand von FET zu TO
-fCO = 1.0 # Ws/K Waermekapazitaet bei TO, Teilbereich des Bleches
-fRO = 20.0 # K/W Waermewiderstand von von TO zu Umgebung: Waermeverlust durch Konvektion und Strahlung
+fTUmgebung = 20.0	# Umgebungstemperatur in Celsius
+fCH = 0.01	# Ws/K	Waermekapazität beim FET, Energie pro Kelvin
+fRH = 25.0	# K/W	Wärmewiderstand beim FET zur Umgebung.
+fRHO = 238.0	# K/W	Waermewiderstand von FET zu TO
+fCO = 1.0	# Ws/K	Waermekapazitaet bei TO, Teilbereich des Bleches
+fRO	= 20.0	# K/W	Waermewiderstand von von TO zu Umgebung: Waermeverlust durch Konvektion und Strahlung
 
 # Linearisierung Optokoppler und Heizung mit FET
 # Berechnung DAC, spaeter vermutlich komplizierter
@@ -16,7 +16,7 @@ fHeatW = 1.0 # Heizleistung Vorgabe in Watt
 
 fFetMaxW = 5.0 # Maximalleiatung
 fFetMaxTeil = 1.0 # Spannung Anteil an DAC für Maximalleistung, 1.0 entspricht z.B. 3.3V
-fFetminW = 0.0 # Minimalleiatung, ist fix bei 0.0
+fFetminW = 0.0 # Minimalleiatung, fix bei 0.0
 fFetminTeil = 0.2 # Spannung Anteil an DAC für Minimalleistung, 0.2 entspricht z.B. 1.0V
 
 fDAC = fHeatW/(fFetMaxW-fFetminW)*(fFetMaxTeil-fFetminTeil)+fFetminTeil
@@ -26,16 +26,16 @@ fDAC = sorted([fFetminTeil, fDAC, fFetMaxTeil])[1]# Wertebereich limitieren, spa
 # Beispiel Modell, Berechnung naechste Temperaturen
 
 # Startwerte
-fTH = fTUmgebung_C # Celsius
-fTO = fTUmgebung_C # Celsius
+fTH = fTUmgebung # Celsius
+fTO = fTUmgebung # Celsius
 fdt = 0.2 # sekunden Verstrichene Zeit seit letzter Berechnung
 
 for counter in range(1000):
 
-    fHLeistungW = fHeatW - (fTH-fTUmgebung_C)/fRH - (fTH-fTO)/fRHO # Leistung zu H
-    fOLeistungW = (fTH-fTO)/fRHO - (fTO-fTUmgebung_C)/fRO # Leistung zu O
-    fTHnew = fTH + (fHLeistungW * fdt/fCH)
-    fTOnew = fTO + (fOLeistungW * fdt/fCO)
+    fHLeistungW = fHeatW - (fTH-fTUmgebung)/fRH - (fTH-fTO)/fRHO # Leistung zu H
+    fOLeistungW = (fTH-fTO)/fRHO - (fTO-fTUmgebung)/fRO # Leistung zu O
+    fTHnew = fTH + (fHLeistungW * fdt / fCH)
+    fTOnew = fTO + (fOLeistungW * fdt / fCO)
 
     fTH = fTHnew
     fTO = fTOnew
